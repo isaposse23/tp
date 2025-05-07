f2305eac91ee5f03228884c6087e73352352f26d
isaposse23 <posseisabella08@gmail.com>
2025-05-07 13:05:26 -0300

    Add files via upload

---
+++ python posse.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+
+def main ():
+    # Crear la ventana principal
+    ventana = tk.Tk ()
+    ventana.title ("Lu es una vaga")
+    ventana.geometry("100x200")
+    ventana.configure (bg="blue")
+    ventana.mainloop ()
+
+if __name__=="__main__":
+    main()
